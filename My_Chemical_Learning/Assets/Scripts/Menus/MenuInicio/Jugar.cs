85880bc23c8beda65a5d2a094b4c3c94103fbec2
Johanna_VJ <165029467+ChavesJohanna@users.noreply.github.com>
2026-06-17 16:00:28 -0300

    Construcción nivel
    Se colocaron los objetos utilizando los sprites del motor para marcar visualmente lo que será el nivel 1

--- My_Chemical_Learning/Assets/Scripts/Menus/MenuInicio/Jugar.cs
+++ My_Chemical_Learning/Assets/Scripts/Menus/MenuInicio/Jugar.cs
@@ -6,6 +6,6 @@ public class Jugar : MonoBehaviour
     public void JugarJuego()
     {
         Debug.Log("Iniciando el juego...");
-        SceneManager.LoadScene("SampleScene"); // Cambiar el nombre segun la escena, Ej: Nivel1, Nivel2, etc.
+        SceneManager.LoadScene("Nivel1"); // Cambiar el nombre segun la escena, Ej: Nivel1, Nivel2, etc.
     }
 }
