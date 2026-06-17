3d57ed16ac4b348df97d90e39483f9d7d05b8b44
MateoTarifa <mtarifa17@gmail.com>
2026-06-17 11:44:21 -0300

    Feature_MenuInicioIntegrado
    Se ha integrado el menú de inicio con el botón de jugar para pasar al primer nivel y el botón de salir para cerrar el juego.

---
+++ My_Chemical_Learning/Assets/Scripts/Menus/MenuInicio/SalirJuego.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class SalirJuego : MonoBehaviour
+{
+    public void SalirDelJuego()
+    {
+        Debug.Log("Saliendo del juego...");
+        Application.Quit();
+    }
+}
